297e51a808abee93d75004ed532e9b549508433c
Lanreath <tyfei00gmail.com>
2021-05-29 17:34:01 +0800

    config left out

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://lanreath:cIpGDt4YAO5kL20l@cluster0.qswkt.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
