3bde165df6de50bd23ea495c1400a466f2819656
Lanreath <tyfei00gmail.com>
2021-06-09 23:35:07 +0800

    Fixed dependencies issues

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://lanreath:cIpGDt4YAO5kL20l@cluster0.qswkt.mongodb.net/merng?retryWrites=true&w=majority",
-  SECRET_KEY: "some very secret key",
-};
